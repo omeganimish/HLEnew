3d512edb970c8cc305db3ed280e87d9c80d7ff73
girikushal <kushal.giri@omegabpo.com>
2025-03-19 18:51:05 +0545

    [main] ui revised header

--- components/Header/index.jsx
+++ components/Header/index.jsx
@@ -30,7 +30,7 @@ const Header = () => {
   return (
     <header
       className={`${
-        barVisible ? "sticky" : ""
+        barVisible ? "sticky" : "relative"
       }  bg-[#ffffff99] top-0 z-10 shadow-[0px_0px_1px_1px_#cbd5c9] py-2 backdrop-blur-md`}
     >
       <Navbar />
